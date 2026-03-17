b9c27546f7b6ad3f6a7f3094fceb6138182ec2e0
Vivin Anitha Thambidurai <268003027+VivinAT04@users.noreply.github.com>
2026-03-17 21:09:55 +0000

    Make randomised function test deterministic

--- tests/test_randomised_function.py
+++ tests/test_randomised_function.py
@@ -1,8 +1,18 @@
 import unittest
+from unittest.mock import patch
 from lecture import randomised_function
+
 
 class MyTestCase(unittest.TestCase):
 
-    def test_randomised_function(self):
-        self.assertEqual('software', randomised_function())  # This will pass or fail randomly
-        # TODO: Can we make this test deterministic? (HINT: Mock testing)
+    @patch("random.randint", return_value=3)
+    def test_randomised_function_returns_software(self, mock_randint):
+        self.assertEqual('software', randomised_function())
+
+    @patch("random.randint", return_value=8)
+    def test_randomised_function_returns_engineering(self, mock_randint):
+        self.assertEqual('engineering', randomised_function())
+
+
+if __name__ == '__main__':
+    unittest.main()
